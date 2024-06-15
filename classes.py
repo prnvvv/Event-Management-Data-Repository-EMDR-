b81177fcba282520d4d9b7633deb56440e6fbfdd
prnvvv <prannavakhanth12@gmail.com>
2024-06-15 19:44:31 +0530

    Update classes.py

--- classes.py
+++ classes.py
@@ -4,7 +4,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="your_password",
+    password="prnv2005",
     database="eventDBMS"
 )
 
