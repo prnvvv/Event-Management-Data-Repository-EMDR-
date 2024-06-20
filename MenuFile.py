2f37b3c229f77943c02ed01b202b68adbe1faa84
prnvvv <prannavakhanth12@gmail.com>
2024-06-20 22:09:56 +0530

    Update MenuFile.py

--- MenuFile.py
+++ MenuFile.py
@@ -2,3 +2,9 @@ from Triggers import CreateTriggers
 from EventsClass import Event
 from AttendeesClass import Attendees
 from RegistrationClass import Registration
+
+print("Welcome to Event Database Management System")
+
+print("MENU")
+
+print("1. ")
