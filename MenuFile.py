6c8ab21e6399b99e9d7f00ef97c7742493a34af5
prnvvv <prannavakhanth12@gmail.com>
2024-06-20 22:20:36 +0530

    Update MenuFile.py

--- MenuFile.py
+++ MenuFile.py
@@ -3,7 +3,11 @@ from EventsClass import Event
 from AttendeesClass import Attendees
 from RegistrationClass import Registration
 
+print()
+
 print("Welcome to Event Database Management System")
+
+print()
 
 print("MENU FOR ACCESSING:")
 
@@ -11,5 +15,20 @@ print("1. Events")
 print("2. Attendees")
 print("3. Registrations")
 
+print()
+
 options = int(input("Enter your desired option: "))
 
+print()
+
+if options == 1:
+    EventObject = Event()
+    print("EVENTS")
+    print()
+    print("MENU")
+    print("1. To create a Events Table (if doesnt exist)")
+    print("2. To add an Event")
+    print("3. To modify an Event")
+    print("4. To delete an event")
+    print("5. To print the details of the Event/Events")
+    print()
